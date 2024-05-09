51ae88563a7fe321bbffed867d30e0196390a474
Julia Alvim <juliameuportoseguro@gmail.com>
2024-05-09 15:57:02 -0300

    projeto finalizado

---
+++ src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+const resultadoMultiplicacao = multiplicacao(5, 3);
+console.log(resultadoMultiplicacao);
+function saudacao(comprimento) {
+    return comprimento;
+}
+const bomDia = saudacao("bom-dia");
+console.log(bomDia);
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+function multiplicacao(a: number, b: number): number {
+    return a * b;
+}
+
+const resultadoMultiplicacao = multiplicacao(5, 3);
+console.log(resultadoMultiplicacao); 
+
+function saudacao(comprimento: string): string {
+    return comprimento;
+}
+
+const bomDia = saudacao("bom-dia");
+console.log(bomDia); 
